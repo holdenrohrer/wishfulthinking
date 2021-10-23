d38444b155ceb56f49aeccb8af102e7c591a3ee2
Vivien <vorellana3@gatech.edu>
2021-10-23 13:50:44 -0400

    added average word and sentence length to function

--- punctuation_analysis.py
+++ punctuation_analysis.py
@@ -1,3 +1,5 @@
+import string
+
 def processFile(bookFile):
     def processLine(line, arr):
         if line[:4] == '*** ':
@@ -8,18 +10,33 @@ def processFile(bookFile):
     return filter(lambda line: processLine(line, betweenStarsArr), bookFile)
 
 def punctuation(iterable):
-    commaCounter = 0
-    sentenceCounter = 0
+    comma_counter = 0
+    sentence_counter = 0
+    word_counter = 0
+    avg_word_len = 0
+    letter_counter = 0
     for line in iterable:
         for i in range(len(line)):
             char = line[i]
             if char == ",":
-                commaCounter +=1
+                comma_counter +=1
             if (char == "." or char == "?" or char == "!") and (line[i+1] == " " or line[i+1] == "\n") and (line[i-3:i] not in [" Mr"," Ms", " Dr", "Mrs"]):
-                sentenceCounter +=1
-    return (commaCounter,sentenceCounter)
+                sentence_counter +=1
+        if line != "\n":
+            line_list = line.split()
+            for word in line_list:
+                letters = False
+                for char in word:
+                    if char in string.ascii_letters:
+                        letter_counter+=1
+                        letters = True
+            if letters == True:
+                word_counter += len(line_list)
+    avg_word_len = letter_counter/word_counter
+    avg_sentence_len = word_counter/sentence_counter
+    return (comma_counter,sentence_counter,word_counter,avg_word_len, avg_sentence_len)
 
-file = open("TestFile.txt", "r", encoding="utf8")
+file = open("TestFile2.txt", "r", encoding="utf8")
 print(punctuation(processFile(file)))
     
 
