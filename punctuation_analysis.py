30772435224c39c5767fa4e835ed0e05c46a32fe
Sean A <33639901+seana02@users.noreply.github.com>
2021-10-23 12:01:52 -0400

    fixed underscore format

--- punctuation_analysis.py
+++ punctuation_analysis.py
@@ -1,25 +1,25 @@
-def processFile(bookFile):
-    def processLine(line, arr):
+def process_file(book_file):
+    def process_line(line, arr):
         if line[:4] == '*** ':
             arr[0] = not arr[0]
             return False # if cur line's stars, don't return
         return arr[0]
-    betweenStarsArr = [False] # stores state of betweenStars as a ref
-    return filter(lambda line: processLine(line, betweenStarsArr), bookFile)
+    between_stars_arr = [False] # stores state of betweenStars as a ref
+    return filter(lambda line: process_line(line, between_stars_arr), book_file)
 
 def punctuation(iterable):
-    commaCounter = 0
-    sentenceCounter = 0
+    comma_counter = 0
+    sentence_counter = 0
     for line in iterable:
         for i in range(len(line)):
             char = line[i]
             if char == ",":
-                commaCounter +=1
+                comma_counter +=1
             if (char == "." or char == "?" or char == "!") and (line[i+1] == " " or line[i+1] == "\n") and (line[i-3:i] not in [" Mr"," Ms", " Dr", "Mrs"]):
-                sentenceCounter +=1
-    return (commaCounter,sentenceCounter)
+                sentence_counter +=1
+    return (comma_counter,sentence_counter)
 
 file = open("TestFile.txt", "r", encoding="utf8")
-print(punctuation(processFile(file)))
+print(punctuation(process_file(file)))
     
 
