bf3ffcc35612d3c62ca6a88d4d72b74e179ca76b
Vivien <vorellana3@gatech.edu>
2021-10-23 11:52:24 -0400

    removed extra files

--- removeCopywrite.py
+++
@@ -1,8 +0,0 @@
-def processFile(bookFile):
-    def processLine(line, arr):
-        if line[:4] == '*** ':
-            arr[0] = not arr[0]
-            return False # if cur line's stars, don't return
-        return arr[0]
-    betweenStarsArr = [False] # stores state of betweenStars as a ref
-    return filter(lambda line: processLine(line, betweenStarsArr), bookFile)
